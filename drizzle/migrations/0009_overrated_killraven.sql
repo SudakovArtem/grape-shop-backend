2b2fd640ed7f997e5b05bce041edc802d69d1763
Artem Sudakov <a.sudakov@inetstudio.ru>
2025-05-29 14:44:37 +0300

    feat: update db

--- drizzle/migrations/0009_overrated_killraven.sql
+++ drizzle/migrations/0009_overrated_killraven.sql
@@ -1,2 +1,6 @@
-ALTER TABLE "products" ALTER COLUMN "cutting_in_stock" SET DATA TYPE integer;--> statement-breakpoint
-ALTER TABLE "products" ALTER COLUMN "seedling_in_stock" SET DATA TYPE integer;
+ALTER TABLE "products" ALTER COLUMN "cutting_in_stock" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "products" ALTER COLUMN "cutting_in_stock" SET DATA TYPE integer USING cutting_in_stock::integer;--> statement-breakpoint
+ALTER TABLE "products" ALTER COLUMN "cutting_in_stock" SET DEFAULT 0;--> statement-breakpoint
+ALTER TABLE "products" ALTER COLUMN "seedling_in_stock" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "products" ALTER COLUMN "seedling_in_stock" SET DATA TYPE integer USING seedling_in_stock::integer;--> statement-breakpoint
+ALTER TABLE "products" ALTER COLUMN "seedling_in_stock" SET DEFAULT 0;
